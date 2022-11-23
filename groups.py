10830a0f1204854d745549cb279462631a11ce49
Shubham-Hazra <92284883+Shubham-Hazra@users.noreply.github.com>
2022-11-23 06:43:21 +0530

    Group working

--- groups.py
+++ groups.py
@@ -1,75 +1,115 @@
 import sqlite3
 
 
-def create_grp_table(grpname,path):
+def create_grp_table(path,grpname,username):
     connection = sqlite3.connect(path)
     cur = connection.cursor()
-    query = '''CREATE TABLE IF NOT EXISTS ?(members TEXT PRIMARY KEY,admin INT)'''
-    cur.execute(query,(grpname,))
-    connection.commit()
-    cur.close()
-    connection.close()
+    try:
+        query = f'''CREATE TABLE {grpname} (usernames TEXT PRIMARY KEY,admin INT)'''
+        cur.execute(query)
+        print(f"Successfully created group {grpname}")
+        query = '''INSERT INTO '''+grpname+''' VALUES(?,?)'''
+        cur.execute(query,(username,1))
+        connection.commit()
+        cur.close()
+        connection.close()
+        return True
+    except:
+        return False
 
-def view_all_members(grpname,path):
+def view_all_members(path,grpname):
     connection = sqlite3.connect(path)
     cur = connection.cursor()
-    cur.execute(f"SELECT members from {grpname}")
+    cur.execute(f"SELECT usernames from {grpname}")
     user_info = cur.fetchall()
-    for i in user_info:
-        print(f"{i[0]}")
+    # for i in user_info:
+    #     print(f"{i[0]}")
+    cur.close()
+    connection.close()
+    return user_info
+
+def drop_table(path,grpname):
+    connection = sqlite3.connect(path)
+    cur = connection.cursor()
+    cur.execute(f"DROP TABLE {grpname}")
     cur.close()
     connection.close()
 
-def insert_to_grp_db(path,grpname,username,isadmin):
+def add_member(path,grpname,username):
     try:
         connection = sqlite3.connect(path)
         cur = connection.cursor()
-        query = '''INSERT INTO users VALUES(?,?,?)'''
-        cur.execute(query,(username,password,status))
-        print(f"Successfully created user {username}")
+        query = f'''INSERT INTO {grpname} VALUES(?,?)'''
+        cur.execute(query,(username,0))
+        print(f"Successfully added user {username} to the group {grpname}")
+        connection.commit()
+        cur.close()
+        connection.close()
         return True
     except:
-        print(f"Failed to create user {username}")
+        print(f"Failed to add user {username} to the group {grpname}")
         return False
 
-def check_username(username,path):
-    #check if the username exist
+def check_admin(path,grpname,username):
     connection = sqlite3.connect(path)
     cur = connection.cursor()
-    cur.execute("SELECT username from users")
-    a = cur.fetchall()
-    if username in a:
-        return True
-    else:
-        return False
-    
-def store_new_info(username, password, status,path):
-    #return True if success register
-    connection = sqlite3.connect(path)
-    cur = connection.cursor()
-    if(check_username(username,path)):
-        cur.close()
-        connection.commit()
-        connection.close()
-        return False
-    else:
-        if insert_to_db(cur,username, password,status):
-            cur.close()
-            connection.commit()
-            connection.close()
+    try:
+        cur.execute(f"SELECT admin from {grpname} WHERE usernames = '{username}'")
+        a = cur.fetchall()
+        # print(a)
+        if len(a) == 0:
+            return False
+        # print(a[0][0])
+        if a[0][0] == 1:
             return True
         else:
             return False
+    except Exception as e:
+        print(e)
+        # print(cur.execute(f"SELECT usernames from {grpname}").fetchall())
+        # print("Error checking admin")
+    # print(username)
+    return False
 
-def show_all_user_info(path):
+def make_admin(path,grpname,username):
     connection = sqlite3.connect(path)
     cur = connection.cursor()
-    cur.execute("SELECT * from users")
-    user_info = cur.fetchall()
-    for i in user_info:
-        print(i)
+    try:
+        cur.execute(f"UPDATE {grpname} SET admin = 1 WHERE usernames = '{username}'")
+    except:
+        print("Error making admin")
     cur.close()
     connection.close()
 
+def delete_member(path,grpname,username):
+    connection = sqlite3.connect(path)
+    cur = connection.cursor()
+    try:
+        cur.execute(f"DELETE from {grpname} WHERE usernames = '{username}'")
+        cur.close()
+        connection.close()
+        return True
+    except:
+        return False
 
-    
+## TODO
+## Make this function work, with only current visible
+## I hab made new function, hopefully it works XD
+def view_all_groups(path, username):
+    connection = sqlite3.connect(path)
+    cur = connection.cursor()
+    cur.execute(f"SELECT name FROM sqlite_master WHERE type='table'")
+    print(cur.fetchall())
+    cur.close()
+    connection.close()
+
+def in_grp(path, grpname, username):
+    connection = sqlite3.connect(path)
+    cur = connection.cursor()
+    result = cur.execute(f"SELECT * FROM {grpname} WHERE usernames = '{username}'").fetchall()
+    cur.close()
+    connection.close()
+    if len(result) == 0:
+        return False
+    else:
+        return True
--- user_info.py
+++ user_info.py
@@ -4,7 +4,7 @@ import bcrypt
 def change_status_online(username,path):
     connection = sqlite3.connect(path)
     cur = connection.cursor()
-    query = '''UPDATE USERS set status = 'ONLINE' where username = ? '''
+    query = '''UPDATE USERS SET status = 'ONLINE' where username = ? '''
     cur.execute(query,(username,))
     connection.commit()
     cur.close()
@@ -13,7 +13,7 @@ def change_status_online(username,path):
 def change_status_offline(username,path):
     connection = sqlite3.connect(path)
     cur = connection.cursor()
-    query = '''UPDATE USERS set status = 'OFFLINE' where username = ? '''
+    query = '''UPDATE users SET status = 'OFFLINE' where username = ? '''
     cur.execute(query,(username,))
     connection.commit()
     cur.close()
@@ -35,23 +35,23 @@ def view_online(path):
     cur.execute("SELECT username from users where status= 'ONLINE'")
     user_info = cur.fetchall()
     for i in user_info:
-        print(i)
+        print(i[0])
     cur.close()
     connection.close()
 
 def create_table(path):
     connection = sqlite3.connect(path)
     cur = connection.cursor()
-    query = '''CREATE TABLE IF NOT EXISTS USERS(username TEXT PRIMARY KEY,salt TEXT,password TEXT, status TEXT)'''
+    query = '''CREATE TABLE IF NOT EXISTS users(username TEXT PRIMARY KEY,salt TEXT,password TEXT, status TEXT, port INT, pub_key TEXT, priv_key TEXT)'''
     cur.execute(query)
     connection.commit()
     cur.close()
     connection.close()
 
-def insert_to_db(cur,username,salt, password, status):
+def insert_to_db(cur,username,salt, password, status, port, pub_key, priv_key):
     try:
-        query = '''INSERT INTO users VALUES(?,?,?,?)'''
-        cur.execute(query,(username,salt,password,status))
+        query = '''INSERT INTO users VALUES(?,?,?,?,?,?,?)'''
+        cur.execute(query,(username,salt,password,status, port, pub_key, priv_key))
         print(f"Successfully created user {username}")
         return True
     except:
@@ -62,24 +62,30 @@ def check_username(username,path):
     #check if the username exist
     connection = sqlite3.connect(path)
     cur = connection.cursor()
-    cur.execute("SELECT username from users")
+    cur.execute("SELECT username from users where username=?",(username,))
     a = cur.fetchall()
-    if username in a:
+    if len(a) != 0:
         return True
     else:
         return False
+    # if username in a:
+    #     return True
+    # else:
+    #     return False
     
-def store_new_info(username, salt,password, status,path):
+def store_new_info(path, username, salt,password, status, port, pub_key, priv_key):
     #return True if success register
     connection = sqlite3.connect(path)
     cur = connection.cursor()
     if(check_username(username,path)):
+        # print("shouldn't be here")
         cur.close()
         connection.commit()
         connection.close()
         return False
     else:
-        if insert_to_db(cur,username, salt,password,status):
+        if insert_to_db(cur,username, salt,password,status, port, pub_key, priv_key):
+            # print("working properly")
             cur.close()
             connection.commit()
             connection.close()
@@ -115,3 +121,92 @@ def check_login_info(username, password, path):
         return True
     else:
         return False
+
+def get_pubkey(path, username):
+    connection = sqlite3.connect(path)
+    cur = connection.cursor()
+    try:
+        key = cur.execute("SELECT pub_key FROM USERS WHERE username=?", (username,)).fetchall()
+        cur.close()
+        connection.close()
+        # print(key[0][0])
+        return key[0][0]
+    except:
+        cur.close()
+        connection.close()
+        return b"-1"
+
+def get_privkey(path, username):
+    connection = sqlite3.connect(path)
+    cur = connection.cursor()
+    try:
+        key = cur.execute("SELECT priv_key FROM USERS WHERE username=?", (username,)).fetchall()
+        cur.close()
+        connection.close()
+        # print(key[0][0])
+        return key[0][0]
+    except:
+        cur.close()
+        connection.close()
+        return b"-1"
+
+def isPortinTable(path, port):
+    connection = sqlite3.connect(path)
+    cur = connection.cursor()
+    if len(cur.execute("SELECT port FROM USERS WHERE port=?", (port,)).fetchall()) != 0:
+        cur.close()
+        connection.close()
+        return True
+    else:
+        cur.close()
+        connection.close()
+        return False
+
+def get_port(path, username):
+    connection = sqlite3.connect(path)
+    cur = connection.cursor()
+    try:
+        key = cur.execute("SELECT port FROM USERS WHERE username=?", (username,)).fetchall()
+        cur.close()
+        connection.close()
+        # print(key[0][0])
+        return key[0][0]
+    except:
+        cur.close()
+        connection.close()
+        return -1
+
+def get_all_active_ports(path):
+    connection = sqlite3.connect(path)
+    cur = connection.cursor()
+    out = cur.execute("SELECT username, port FROM USERS WHERE status = 'ONLINE' ").fetchall()
+    cur.close()
+    connection.close()
+    return out
+
+def check_username_online(path, username):
+    connection = sqlite3.connect(path)
+    cur = connection.cursor()
+    out = cur.execute("SELECT * FROM USERS WHERE status = 'ONLINE' AND username=?", (username,)).fetchall()
+    cur.close()
+    connection.close()
+    if len(out) != 0:
+        return True
+    else:
+        return False
+
+def check_status(path, username):
+    connection = sqlite3.connect(path)
+    cur = connection.cursor()
+    out = cur.execute("SELECT status FROM USERS WHERE username=?", (username,)).fetchall()
+    cur.close()
+    connection.close()
+    return out
+
+def update_port(path, username, port):
+    connection = sqlite3.connect(path)
+    cur = connection.cursor()
+    cur.execute(f"UPDATE USERS SET port = {port} WHERE username=?", (username,))
+    connection.commit()
+    cur.close()
+    connection.close()
