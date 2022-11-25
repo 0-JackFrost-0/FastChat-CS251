697c7b6156e188753854aa3a2f61bbe2670166d9
Shubham-Hazra <92284883+Shubham-Hazra@users.noreply.github.com>
2022-11-25 23:21:53 +0530

    Performance analysis

--- performance.py
+++ performance.py
@@ -1,45 +1,53 @@
 from subprocess import Popen, PIPE
 import time
+from time import sleep
 import pwnlib
 import pwn
+import sys
 
-
-no_servers = 4
-no_clients = 100
+no_clients = int(sys.argv[1])
 
 start = time.process_time()
 
 process = pwn.process
-balancer = process(["python3", "load_balancer.py"],
-                   stdout=PIPE, stderr=PIPE, stdin=PIPE)
+# balancer = process(["python3", "load_balancer.py",f"{no_servers}","5555"],
+#                    stderr=PIPE, stdin=PIPE)
 
-for x in range(1, no_servers + 1):
-    globals()['server%s' % x] = process(["python3", "server.py",
-                                         "127.0.0.1", f"500{x}"], stdout=PIPE, stderr=PIPE, stdin=PIPE)
+# for x in range(1, no_servers + 1):
+#     globals()['server%s' % x] = process(["python3", "server.py",
+#                                          "127.0.0.1", f"500{x}"], stdout=PIPE, stderr=PIPE, stdin=PIPE)
 
 for x in range(1, no_clients + 1):
     globals()['client%s' % x] = process(["python3", "client.py",
-                                         "127.0.0.1", "5555"], stdout=PIPE, stderr=PIPE, stdin=PIPE)
+                                         "127.0.0.1", "5555"], stderr=PIPE, stdin=PIPE)
 
 for x in range(1, no_clients + 1):
     globals()['client%s' % x].sendline(b"2")
-    globals()['client%s' % x].sendline(("user"+ str(x)).encode("UTF-8"))
-    globals()['client%s' % x].sendline(("pwd"+ str(x)).encode("UTF-8"))
-    
-    globals()['client%s' % x].sendline(("hello").encode("UTF-8"))
-    globals()['client%s' % x].sendline(("i").encode("UTF-8"))
-    globals()['client%s' % x].sendline(("user1").encode("UTF-8"))
-    
+    sleep(0.5)
+    globals()['client%s' % x].sendline(("user"+ str(x)).encode())
+    sleep(0.5)
+    globals()['client%s' % x].sendline(("pwd"+ str(x)).encode())
+    sleep(0.5)
+    # b"" == globals()['client%s' % x].recvline()
+
     # globals()['client%s' % x].sendline(("\\quit").encode("UTF-8"))
     
-
-
+for x in range(1, no_clients + 1):
+    for x in range(1, no_clients + 1):
+            globals()['client%s' % x].sendline(("hello").encode())
+            sleep(0.5)
+            globals()['client%s' % x].sendline(("i").encode())
+            sleep(0.5)
+            globals()['client%s' % x].sendline((f"user{(x+1)%no_clients}").encode())
+            sleep(0.5)
 for x in range(no_clients, 0, -1):
     globals()['client%s' % x].kill()
-for x in range(no_servers):
-    globals()['server%s' % (no_servers - x)].kill()
-balancer.kill()
 
 stop = time.process_time()
 
 print(stop-start)
+
+# process(["rm", "-rf","databases"],
+#                    stderr=PIPE, stdin=PIPE)
+# process(["mkdir","databases"],
+#                    stderr=PIPE, stdin=PIPE)
